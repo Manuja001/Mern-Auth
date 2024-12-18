db5e1b2f1b653b60686a1c8da112d138c8531bff
Manuja Sandeep <it22221100@my.sliit.lk>
2024-12-19 00:57:18 +0530

    Connected to Mongo db via the .env file

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
 
 const app = express();
 
