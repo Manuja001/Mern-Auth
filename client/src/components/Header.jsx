385da25415669883dd9c65726dab5bb9b293b367
Manuja Sandeep <it22221100@my.sliit.lk>
2024-12-18 23:52:17 +0530

    Complete Header and the header navigations to pages

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+function Header() {
+  return (
+    <div className="bg-slate-200 py-2 px-10 shadow-sm flex justify-between items-center">
+      <div>
+        <h1>
+          <span className="text-3xl text-slate-800 font-bold">Auth</span>{" "}
+          <span className="text-2xl text-slate-500 font-semibold">App</span>{" "}
+          <img
+            src={logo}
+            alt="logo"
+            className="w-10 h-10 inline rounded-full items-center"
+          />
+        </h1>
+      </div>
+      <div className="font-semibold">
+        <nav>
+          <ul className="flex gap-5">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/signin">Sign In</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
