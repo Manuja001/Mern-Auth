36aa4e44f28d9c55b74b68ca58ce5f1bd7cbb703
Manuja Sandeep <it22221100@my.sliit.lk>
2024-12-21 14:39:57 +0530

    create signup route and encrypted the passward using bycryptjs

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // encrypt the password using bcryptjs
+
+  const hashedPassword = bcryptjs.hashSync(password, 12);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  // add try-catch block to handle errors
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "Signup successful!" });
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
@@ -23,3 +25,4 @@ app.listen(5000, () => {
 // creating the routes
 
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
